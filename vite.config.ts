9887f984ee7580ea7ed67ab5e387ce59097da223
SatooRu65536 <ryuto65536@gmail.com>
2026-01-17 16:01:13 +0900

    chore: react compiler を有効化

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig(() => ({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
   ],
   clearScreen: false,
   server: {
